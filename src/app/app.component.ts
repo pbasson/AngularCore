a6554e9f08348ca9f641eb0939cb1382666d0d9f
preetpal basson <preet.basson@gmail.com>
2024-11-21 15:01:18 +0000

    0.05: Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'User';
   successMsg = 'Congratulations! Your app is running. 🎉';
   newLine = "This Phantom Preet the new ruler !!!!!";
+  
+  dateMsg: Date = new Date();
+  todayDate: Date = new Date();
 }
