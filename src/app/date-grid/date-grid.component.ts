06fdb0c3c055bd4a82b143a5c42f2bd703ced0a2
preetpal basson <preet.basson@gmail.com>
2024-12-05 15:37:32 +0000

    0.29: Update

--- src/app/date-grid/date-grid.component.ts
+++ src/app/date-grid/date-grid.component.ts
@@ -1,14 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { DateClass } from '../../models/date-class.model';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-date-grid',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatPaginatorModule],
   templateUrl: './date-grid.component.html',
   styleUrl: './date-grid.component.css'
 })
 export class DateGridComponent {
   @Input({ required:true }) dataSource!: DateClass[];
-       
+  displayedColumns: string[] = ['Index', 'Day', 'Date', 'Time', 'Temp'];     
 }
